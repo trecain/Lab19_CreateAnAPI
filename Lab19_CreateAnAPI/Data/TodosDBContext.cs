cf7da467db8e52e3c2650bcd56fc96a71b739289
trecain <developedbycain@yahoo.com>
2018-11-15 21:43:12 -0800

    added a empty database and set up the startup file

---
+++ Lab19_CreateAnAPI/Data/TodosDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab19_CreateAnAPI.Data
+{
+    public class TodosDBContext : DbContext
+    {
+        public TodosDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Lab19_CreateAnAPI/Startup.cs
+++ Lab19_CreateAnAPI/Startup.cs
@@ -6,15 +6,32 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Lab19_CreateAnAPI.Data;
 
 namespace Lab19_CreateAnAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            var cb = new ConfigurationBuilder().AddEnvironmentVariables();
+            cb.AddUserSecrets<Program>();
+            configuration = cb.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<TodosDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +42,8 @@ namespace Lab19_CreateAnAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
